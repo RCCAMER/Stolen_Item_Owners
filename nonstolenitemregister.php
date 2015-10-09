f7e1c329ec48dfb94894528c97468893cd04d5b6
RCCAMER <rcameron@sycamoreleaf.com>
2015-10-09 16:49:26 -0500

    fixed issue with insert

--- nonstolenitemregister.php
+++ nonstolenitemregister.php
@@ -36,10 +36,6 @@ button{
     print("<br>");
 
     print("<FORM name=theForm action='nonstolenitemregister.php' method=POST>");
-    print("<INPUT type=hidden name=item value=$item >");
-    print("<INPUT type=hidden name=item value=$serial_number >");
-    print("<INPUT type=hidden name=item value=$info >");
-    print("<INPUT type=hidden name=item value=$desc >");
     print("<INPUT type=hidden name=task value=insert >");
 
     print("<table style='width:100%;text-color:white;'>");
@@ -57,7 +53,7 @@ button{
     print("<td style='width:15%;border-top:1px solid red;border-bottom:1px solid red;'><input type='text' name='item' value='$item' ></td>");
     print("<td style='width:10%;border-top:1px solid red;border-left:1px solid red;border-bottom:1px solid red;padding-left:5px;'><input type='text' name='serial_number' value='$serial_number' maxlength='16' size='16'></td>");
    // print("<td style='width:20%;border-top:1px solid red;border-bottom:1px solid red;'> <input type='file' name='pic' accept='image/'></td>");
-    print("<td style='width:25%;border-top:1px solid red;border-bottom:1px solid red;'><textarea type='text' name='contact information' rows='5' cols='50' maxlength='500'>$info </textarea></td>");
+    print("<td style='width:25%;border-top:1px solid red;border-bottom:1px solid red;'><textarea type='text' name='contact_information' rows='5' cols='50' maxlength='500'>$info </textarea></td>");
     print("<td style='width:40%;border-top:1px solid red;border-bottom:1px solid red;border-right:1px solid red;'><textarea type='text' name='description' rows='5' cols='50' maxlength='500' >$desc </textarea></td>");
     print("</tr>");
     print("</table>");
@@ -70,14 +66,18 @@ button{
 ////////////////////////////////////////////////////
 ///TASK
 //////////////////////////////////////////////////
+$task = $_POST['task'];
+$item = $_POST['item'];
+$serial_number = $_POST['serial_number'];
+$info = $_POST['contact_information'];
+$desc = $_POST['description'];
 
 if($task == 'insert'){
-
-$sql = "INSERT INTO Registration (Item,Serial_Number,Picture,Contact_Info,Description) VALUES ($item,$serial_number,0,$info,$desc)";
+$sql = "INSERT INTO Registration (Item,Serial_Number,Picture,Contact_Info,Description) VALUES ('$item','$serial_number',0,'$info','$desc')";
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
 }
 
 
